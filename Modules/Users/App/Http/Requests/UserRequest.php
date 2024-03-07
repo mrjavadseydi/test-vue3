df9f1b1ca0cfe78a046a525f62a96ac288a0ada0
mohammad javad seydi <seydi.birjand@gmail.com>
2024-03-08 02:45:01 +0330

    Update UserRequest.php

--- Modules/Users/App/Http/Requests/UserRequest.php
+++ Modules/Users/App/Http/Requests/UserRequest.php
@@ -3,6 +3,7 @@
 namespace Modules\Users\App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UserRequest extends FormRequest
 {
@@ -25,10 +26,11 @@ class UserRequest extends FormRequest
         $allowed_roles = allowed_roles();
         ///if the request is for updating a user password can be empty
         $is_update = request()->method() === 'PUT';
+        $uniq =$is_update ? Rule::unique('users')->ignore($this->id) : 'unique:users';
         return [
             'first_name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email'.$is_update ? ','.$this->id :'',
+            'email' => ['required','email',$uniq],
             'mobile' => 'required|string|max:255',
             'role' => ['required', 'string', 'max:255', 'in:'.implode(',',$allowed_roles)],
             'password' => [$is_update?"nullable":"required", "string", "min:8"],
